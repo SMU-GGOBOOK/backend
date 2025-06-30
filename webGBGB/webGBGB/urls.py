ea4231a73616394c8c7d12d3263cd5d0fc20e2bc
sharehyun <lkskr7251@gmail.com>
2025-06-30 15:48:39 +0900

    앱 이름 home -> base로 변경함

--- webGBGB/webGBGB/urls.py
+++ webGBGB/webGBGB/urls.py
@@ -3,7 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
