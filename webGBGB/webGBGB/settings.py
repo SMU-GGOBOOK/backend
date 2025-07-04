34a3bc19fae13ca0f7b5e79f661cef226e88a841
sharehyun <lkskr7251@gmail.com>
2025-07-04 18:39:25 +0900

    booksearch 저장

--- webGBGB/webGBGB/settings.py
+++ webGBGB/webGBGB/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'base',
     'cscenter',
     'home',
+    'booksearch',
 
 ]
 
--- webGBGB/webGBGB/urls.py
+++ webGBGB/webGBGB/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('cscenter/', include('cscenter.urls')),
+    path('booksearch/', include('booksearch.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
