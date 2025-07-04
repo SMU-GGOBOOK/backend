bc50c77409a849e208b1cc032b09f310beb895e6
resader1 <resader40@gmail.com>
2025-07-04 09:58:15 +0900

    전체
    일단..

--- webGBGB/webGBGB/urls.py
+++ webGBGB/webGBGB/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('member.urls')),
-    path('', include('home.urls')),
+    path('', include('dummyhome.urls')),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
