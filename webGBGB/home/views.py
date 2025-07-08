2d1bebc4cf1784085c3f1737aa5f1ab948aad5e9
sharehyun <lkskr7251@gmail.com>
2025-07-08 16:49:35 +0900

    메인페이지 top5출력

--- webGBGB/home/views.py
+++ webGBGB/home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from booksearch.models import Book
 
 def index(request):
-    return render(request,'index.html')
+    review_top5 = Book.objects.all().order_by('-review_count')[:5]
+    bookmark_top5 = Book.objects.all().order_by('-bookmark_count')[:5]
+    views_top5 = Book.objects.all().order_by('-views')[:5]
+    
+    context = {'review':review_top5,'bookmark':bookmark_top5,'views':views_top5}
+    
+    return render(request,'index.html',context)
