0052ff20260c4eb39a7d37ab23ef31745042e231
hwk3005 <hwk3005@naver.com>
2025-07-09 20:50:06 +0900

    250709 백엔드_그룹만들기 백엔드 구현 완료(도서api연결, Form제출시 DB최종저장)
    1. 그룹만들기
      1) 책 선택-모달창
        - 도서 api 연결
        - 책제목/저자 등 키워드 검색 시 검색목록 표시
        - 책 선택버튼 클릭 시 모달창 닫히고 책 선택됨 (변경도 가능)
      2) Form제출 시 shareMain-ReadingGroup db에 최종 저장
    
    2. 메인페이지
      1) 참여중인 그룹
        - 만든 그룹 추가
        - 책 이미지 안 띄워지던 오류 해결
      2) 그룹 목록
        - 만든 그룹 최신순으로 정렬
        - 해시태그, 책 이미지 안 띄워지던 오류 해결

--- webGBGB/shareMain/urls.py
+++ webGBGB/shareMain/urls.py
@@ -5,5 +5,5 @@ app_name = 'shareMain'
 urlpatterns = [
     path('Share_Main/', views.Share_Main, name='Share_Main'),
     path('Share_AddGroup/', views.Share_AddGroup, name='Share_AddGroup'),
-
+    path("shareMain/ajax_search/", views.ajax_search, name="ajax_search"),
 ]
--- webGBGB/webGBGB/settings.py
+++ webGBGB/webGBGB/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'shareMain',
-    
+    'booksearch',
 
 ]
 
