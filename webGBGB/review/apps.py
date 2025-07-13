d4cfd8a9d1f9d42ee8efe9f95df03e853d6e2ee4
kb4954 <kb4954@naver.com>
2025-07-13 20:51:09 +0900

    feat: 수정

--- webGBGB/review/apps.py
+++ webGBGB/review/apps.py
@@ -1,12 +1,10 @@
 from django.apps import AppConfig
 
 
-<<<<<<<< HEAD:webGBGB/review/apps.py
 class ReviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'review'
-========
+
 class SharemainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shareMain'
->>>>>>>> 7-feat-share_main-addgroup-backend:webGBGB/shareMain/apps.py
