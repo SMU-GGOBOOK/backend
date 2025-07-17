90c50cd258c6472fe7a565667c7b3c6ace1d8b7d
kb4954 <kb4954@naver.com>
2025-07-17 17:09:46 +0900

    feat: db

--- webGBGB/webGBGB/settings.py
+++ webGBGB/webGBGB/settings.py
@@ -75,7 +75,7 @@ SITE_ID = 1 # 필수 설정 (없으면 추가)
 
 
 KAKAO_JAVASCRIPT_KEY = 'bec1572a47f03392aec44dd28bce95b4' # <-- Kakao JavaScript Key
-KAKAO_REDIRECT_URI = 'http://localhost:8000/member/kakao/callback/'
+KAKAO_REDIRECT_URI = 'http://127.0.0.1:8000/member/kakao/callback/'
 KAKAO_REST_API_KEY = 'afb77164fe046904554115dc84fccb93'
 
 
